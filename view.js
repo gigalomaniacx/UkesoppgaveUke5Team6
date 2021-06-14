171de76c3abe33b2832a690d1db94d242c43e964
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2021-06-14 12:40:39 +0200

    Update view.js

--- view.js
+++ view.js
@@ -1,15 +1,18 @@
 updateView()
 function updateView(){
     document.getElementById('app').innerHTML = `
-        <h1 onclick="chooseType()">supre duper stein saks papper</h1>
-        <button class="button" id="stein" value="🥌" onclick="playerSelect = this.value;checkWinCondition()">stein</button>
-        <button class="button" id="saks" value="✂" onclick="playerSelect = this.value;checkWinCondition()">saks</button>
-        <button class="button" id="papir" value="📄" onclick="playerSelect = this.value;checkWinCondition()">papir</button><br/>
-        ${compResponse}<br/>
-        ${response}<br/>
-        ${playerSelect}<br/>
-        ${viewWins}
-        ${viewLosses}
-        ${viewDraws}
+        <h1 onclick="chooseType()">Supre Duper Stein Saks Papper</h1>
+        <div class="wrap">
+        <button class="button" id="stein" value="🥌" onclick="playerSelect = this.value;checkWinCondition()">Stein</button>
+        <button class="button" id="saks" value="✂" onclick="playerSelect = this.value;checkWinCondition()">Saks</button>
+        <button class="button" id="papir" value="📄" onclick="playerSelect = this.value;checkWinCondition()">Papir</button><br/>
+        </div>
+        <p class=ikon1>${playerSelect}</p><br/>
+        <p class=svar>${response}</p><br/>
+        <p class=ikon1>${compResponse}</p><br/>
+        <p class=infotxt>${viewWins}</p>
+        <p class=infotxt>${viewLosses}</p>
+        <p class=infotxt>${viewDraws}</p>
+       
     `;
 }
